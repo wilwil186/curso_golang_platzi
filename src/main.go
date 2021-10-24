023691a4672c22c1b28afff117319d1c88cd0826
wilwil186 <edujerezwilson@gmail.com>
2021-10-24 18:10:18 -0500

    fmt

--- src/main.go
+++ src/main.go
@@ -2,11 +2,26 @@ package main
 
 import "fmt"
 
-func main() { //Declaración de variables
-	helloMessage := "hello"
-	woldMessage := "world"
-	//println
-	fmt.Println(helloMessage, woldMessage)
-}
+func main() {
+	// Declaración de variables
+	helloMessage := "Hello"
+	worldMesssage := "world"
 
-//para comentar
+	// Println
+	fmt.Println(helloMessage, worldMesssage)
+	fmt.Println(helloMessage, worldMesssage)
+
+	// Printf
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
+
+	// Sprintf
+	message := fmt.Sprintf("%s tiene más de %d cursos", nombre, cursos)
+	fmt.Println(message)
+
+	// Tipo datos
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
+}
