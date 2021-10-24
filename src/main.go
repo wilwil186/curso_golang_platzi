92f5e99af0feabfb0377ba2303334cad14335adf
wilwil186 <edujerezwilson@gmail.com>
2021-10-24 18:38:48 -0500

    first uso de funciones

--- src/main.go
+++ src/main.go
@@ -3,26 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// Declaración de variables
-	helloMessage := "Hello"
-	worldMesssage := "world"
-
-	// Println
-	fmt.Println(helloMessage, worldMesssage)
-	fmt.Println(helloMessage, worldMesssage)
-
-	// Printf
-	nombre := "Platzi"
-	cursos := 500
-	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
-	//pole solo %v cuando no sepas el tipo de dato
-	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
-
-	// Sprintf
-	message := fmt.Sprintf("%s tiene más de %d cursos", nombre, cursos)
-	fmt.Println(message)
-
-	// Tipo datos
-	fmt.Printf("helloMessage: %T\n", helloMessage)
-	fmt.Printf("cursos: %T\n", cursos)
+	fmt.Println("hola mundo")
+	fmt.Println("hola mundo2")
+	fmt.Println("hola mundo3")
 }
