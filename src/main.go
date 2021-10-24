0be83239d0333c9353d6d64b036a4aa8a6259bab
wilwil186 <edujerezwilson@gmail.com>
2021-10-24 15:17:14 -0500

    datos_primitidos

--- src/main.go
+++ src/main.go
@@ -2,32 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
-	// Declaración de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.14
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-	//Declaración de variables enteras
-	//coloca los dos puntos cuando la variable no a sido creada con anterioridad
-	base := 12
-	//otra forma es
-	var altura int = 14
-	//otra forma
-	var area int
-
-	fmt.Println(base, altura, area) //se necesita utulizar toda variable que se declara.
-	//Zero values.
-	//por defecto estos valores tienen.
-	var a int     //0
-	var b float64 //0
-	var c string  //string vacio
-	var d bool    // false
-	fmt.Println(a, b, c, d)
-	//ejercicio Area de un cuadrado
+func main() { //ejercicio Area de un cuadrado
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println(areaCuadrado)
+
+	x := 10
+	y := 58
+
+	//suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	//Resta
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	//multiplicación
+	result = x * y
+	//División
+	result = y / x
+	fmt.Println("División:", result)
+	//modulo
+	result = y % x
+	fmt.Println("Modulo:", result)
+	//Incremental
+	x++
+	fmt.Println("Incremental:", x)
+	//Decremental}
+	x--
+	fmt.Println("Decremental:", x)
 
 }
 
