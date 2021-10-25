b3b104f4512b98b05de67219c8e52077ad477e5d
wilwil186 <edujerezwilson@gmail.com>
2021-10-24 19:53:01 -0500

    fin

--- src/main.go
+++ src/main.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+func normalFunction(message string) {
+	fmt.Println(message)
+}
+func tripeArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+func returnValue(a int) int {
+	return a * 2
+}
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
 func main() {
-	fmt.Println("hola mundo")
-	fmt.Println("hola mundo2")
-	fmt.Println("hola mundo3")
+	normalFunction("Hola Mundo")
+	tripeArgument(1, 2, "hola")
+
+	value := returnValue(2)
+	fmt.Println("Value:", value)
+
+	value1, value2 := doubleReturn(2)
+	fmt.Println(value1, value2)
 }
