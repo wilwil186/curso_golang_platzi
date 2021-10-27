1d9d99d047b2287d681380e9c1f62f011204314a
wilwil186 <edujerezwilson@gmail.com>
2021-10-27 14:04:25 -0500

    fin de clase

--- src/main.go
+++ src/main.go
@@ -3,23 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	//utiliza esto cuando vas a iterar sobre una misma variable
-	switch modulo := 4 % 2; modulo {
-	case 0:
-		fmt.Println("Es par")
-	default:
-		fmt.Println("Es impar")
-	}
-	// Sin condicion
-	// cuando quieres anidar multiples condiciones.
-	value := 200
-	switch {
-	case value > 100:
-		fmt.Println("el valor es mayor que 100")
-	case value < 0:
-		fmt.Println("el valor es menor que 0")
-	default:
-		fmt.Println("we no cumple ninguna condición")
+	//Defer
+	defer fmt.Println("hola")
+	fmt.Println("mundo")
+	//continue y break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		// Continue
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+		// Break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
 	}
 }
 
