c076d5e288c8c5a0e1b40170764588320001e15d
wilwil186 <edujerezwilson@gmail.com>
2021-10-27 18:27:51 -0500

    fin de la clase

--- src/main.go
+++ src/main.go
@@ -3,23 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	//Defer
-	defer fmt.Println("hola")
-	fmt.Println("mundo")
-	//continue y break
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		// Continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
-		// Break
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
-	}
+	//Array
+	var Array [4]int
+	Array[0] = 1
+	Array[1] = 2
+	fmt.Println(Array, len(Array), cap(Array))
+
+	//slide
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap((slice)))
+
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+	//no se puede añadir elementos sen los array
+	//por que los arrays son inmutables y los dlide no
+	//para eso utilizamos el comando Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+	//cuando necesitamos agregar una lista
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
+	//arrays inmutables, slice no inmutables sin embargo ya que los arrays son inmutables son mas eficientes
 }
 
 //cuando estas ejecutando muntiples funciones if utilizas switch
