45a5dd6da09e3b31b0b567aaf288d788e99cf6e7
wilwil186 <edujerezwilson@gmail.com>
2021-10-31 10:41:41 -0500

    fin de la clase

--- src/main.go
+++ src/main.go
@@ -4,44 +4,16 @@ import "fmt"
 
 type pc struct {
 	ram   int
-	disk  int
 	brand string
+	disk  int
 }
 
-func (myPC pc) ping() {
-	fmt.Println(myPC.brand, "Pong")
-}
-
-func (myPC *pc) duplicateRAM() {
-	myPC.ram = myPC.ram * 2
-
+func (myPC pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPC.ram, myPC.disk, myPC.brand)
 }
 
 func main() {
-
-	a := 50
-	b := &a
-
-	fmt.Println(b)
-	// el * es la operación contraria de &
-	fmt.Println(*b)
-
-	*b = 100
-	fmt.Println(a)
-
-	//instanciar
-	myPC := pc{ram: 16, disk: 200, brand: "msi"}
-	fmt.Println(myPC)
-	//retornar un mensaje aletorio
-	myPC.ping()
-	//estado actual
-	fmt.Println(myPC)
-	//suplicar
-	myPC.duplicateRAM()
+	myPC := pc{ram: 16, brand: "msi", disk: 100}
 
 	fmt.Println(myPC)
-	myPC.duplicateRAM()
-
-	fmt.Println(myPC)
-
 }
