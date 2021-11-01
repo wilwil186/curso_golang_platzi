27d4aa46d1452e1674487b3e077c260b2edc55b6
wilwil186 <edujerezwilson@gmail.com>
2021-11-01 10:39:05 -0500

    copia

--- src/main.go
+++ src/main.go
@@ -1,32 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func say(text string, wg *sync.WaitGroup) {
-
-	defer wg.Done()
-
-	fmt.Println(text)
+func say(text string, c chan<- string) {
+	c <- text
 }
 
 func main() {
-	var wg sync.WaitGroup
+	c := make(chan string, 1)
 
 	fmt.Println("Hello")
-	wg.Add(1)
 
-	go say("world", &wg)
+	go say("Bye", c)
 
-	wg.Wait()
-
-	go func(text string) {
-		fmt.Println(text)
-	}("Adios")
-
-	time.Sleep(time.Second * 1)
-
+	fmt.Println(<-c)
 }
