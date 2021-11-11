dcc0810e15ecd74059d8aa65b7fab61588bd7a52
wilwil186 <edujerezwilson@gmail.com>
2021-11-11 07:03:17 -0500

    revisión

--- src/main.go
+++ src/main.go
@@ -14,9 +14,11 @@ func say(text string, wg *sync.WaitGroup) {
 }
 
 func main() {
+	// este paquete te permite interartuar con las gorutines
 	var wg sync.WaitGroup
 
 	fmt.Println("Hello")
+	//vamos a agregar una gorutine
 	wg.Add(1)
 	//para caplicar las gorutines
 	go say("world", &wg)
@@ -26,7 +28,7 @@ func main() {
 	go func(text string) {
 		fmt.Println(text)
 	}("Adios")
-
+	// espera we
 	time.Sleep(time.Second * 1)
 
 }
